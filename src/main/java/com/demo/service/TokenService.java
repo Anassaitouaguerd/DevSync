3c86ea95f52464047fa72b2da42dd9b011d0f78c
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-10-14 17:17:29 +0100

    Add methods to find and update tokens in TokenRepository
    Introduced `findActiveTokenByUserAndType` to retrieve active tokens based on user and type. Added `updateToken` to enable token updates. These changes enhance the token management capabilities within the repository.

--- src/main/java/com/demo/service/TokenService.java
+++ src/main/java/com/demo/service/TokenService.java
@@ -9,24 +9,44 @@ import org.apache.commons.lang3.RandomStringUtils;
 import java.util.Optional;
 
 public class TokenService {
+    private TokenRepository tokenRepository;
+    private UserRepository userRepository;
+
+    public TokenService() {
+        this.tokenRepository = new TokenRepository();
+        this.userRepository = new UserRepository();
+    }
+
     private String generateToken(){
         return RandomStringUtils.randomAlphanumeric(32);
     }
+
     public void createToken(Long userId) {
-        Optional<User> userOptional = new UserRepository().findUserById(userId);
+        Optional<User> userOptional = userRepository.findUserById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             for (int i = 0; i < 2; i++) {
                 String uniqueToken = generateToken();
                 Token token = new Token(uniqueToken, user, "remplacer");
-                new TokenRepository().createToken(token);
+                tokenRepository.createToken(token);
             }
             String uniqueToken = generateToken();
             Token token = new Token(uniqueToken, user, "suppression");
-            new TokenRepository().createToken(token);
+            tokenRepository.createToken(token);
         } else {
             throw new RuntimeException("User not found with id: " + userId);
         }
+    }
+
+    public boolean useToken(Long userId, String tokenType) {
+        User user = userRepository.findUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Token token = tokenRepository.findActiveTokenByUserAndType(user, tokenType);
+        if (token == null) {
+            return false;
+        }
+        token.setStatus("used");
+        tokenRepository.updateToken(token);
+        return true;
     }
 
     public void deleteToken() {
